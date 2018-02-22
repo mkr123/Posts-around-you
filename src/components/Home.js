087aaed816e1721718f028bc9c592601805184b8
mkr123 <mankairui@yahoo.com>
2018-02-22 09:51:28 -0800

    final code

---
+++ src/components/Home.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Tabs, Button,Spin } from 'antd';
+import {GEO_OPTIONS} from "../constants"
+
+const TabPane = Tabs.TabPane;
+const operations = <Button>Extra Action</Button>;
+
+
+export class Home extends React.Component {
+    state = {
+        loadingGeoLocation: false,
+    }
+    componentDidMount(){
+        this.setState({loadingGeoLocation: true, error: ''});
+        this.getGeoLocation();
+    }
+    getGeoLocation = () => {
+    if ("geolocation" in navigator) {
+
+        navigator.geolocation.getCurrentPosition(
+            this.onSuccessLoadGeoLocation,
+            this.onFailedLoadGeoLocation,
+            GEO_OPTIONS,
+        );
+    } else {
+       this.setState({error: "Browser does not support Geolocation!"})
+    }
+    }
+    onSuccessLoadGeoLocation = (position) =>{
+    console.log(position);
+        this.setState({loadingGeoLocation: false, error:''});
+    }
+    onFailedLoadGeoLocation = () =>{
+        this.setState({loadingGeoLocation: false, error: "Failed to load geo location!"});
+    }
+    getGalleryPanelContent = () =>{
+        if(this.state.error){
+            return <div>{this.state.error}</div>;
+        }
+        else if(this.state.loadingGeoLocation) {
+            return <Spin tip="Loading geo location"></Spin>;
+        }
+    }
+    render() {
+        return (
+            <Tabs tabBarExtraContent={operations}
+                className="main-tabs"
+            >
+            <TabPane tab="Posts" key="1">
+                {
+                    this.getGalleryPanelContent()
+                }
+            </TabPane>
+
+
+            <TabPane tab="Map"  key="2">Content of tab 2</TabPane>
+
+            </Tabs>
+
+        );
+    }
+}
